c4ac759988a39eb189e8124da8911a7e93929319
sgujuluva <sanghee04@gmail.com>
2022-08-14 13:11:41 +0200

    animation done about title

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,7 +5,22 @@ import "./Intro.css";
 function Intro() {
   return (
     <div className="intro-container">
-     <div className="intro-left">left</div>
+     <div className="intro-left">
+        <h2 className="intro-title">Hi, I am </h2>
+        <h1 className="intro-name">Sangeetha Gujuluva</h1>
+        <div className="my-title">
+            <div className="my-title-wrapper">
+                <div className="my-title-item">Web Developer</div>
+                <div className="my-title-item">
+                    Passionate Cook
+                </div>
+                <div className="my-title-item">
+                    Gardener
+                </div>
+                <div className="my-title-item">Event Organiser</div>
+            </div>
+        </div>
+     </div>
      <div className="intro-right">right</div>
     </div>
   )
