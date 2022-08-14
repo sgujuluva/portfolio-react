f2c3d685efc040ff92b1f085b2a0aadc20761e62
sgujuluva <sanghee04@gmail.com>
2022-08-14 19:18:48 +0200

    home and about me page styled

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -21,7 +21,7 @@ function Intro() {
           <p className="my-desc">
             I am new bee in developing websites and have been loving every
             minute of it. I am student in Digital Carrier Institute, Germany
-            currently doing REACT and Node JS{" "}
+            currently doing REACT and Node JS
           </p>
           <svg className="intro-rightarrow" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16"> <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/> </svg>
         </div>
