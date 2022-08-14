1fc42279f225d263cf8a46f4a68519326a2a61ae
sgujuluva <sanghee04@gmail.com>
2022-08-14 12:48:26 +0200

    intro component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+//components
+import Intro from "./components/intro/Intro";
+//styles
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+  <Intro/>
     </div>
   );
 }
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+//style
+import "./Intro.css";
+
+function Intro() {
+  return (
+    <div>Intro</div>
+  )
+}
+
+export default Intro
