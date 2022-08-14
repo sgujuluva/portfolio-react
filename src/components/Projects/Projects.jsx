3de6d7bbca6905e0d2d786d994f30c70d06f2108
sgujuluva <sanghee04@gmail.com>
2022-08-14 21:23:52 +0200

    project header styled

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from "react";
 //styles
-import "./Projects.css"
+import "./Projects.css";
 
 function Projects() {
   return (
-    <div className = "project-list">
-        <div className="project-list-text">
-<h1 className="project-title">My Projects</h1>
-<p className="project-desc">Here are my Projects. Projects are done in HTML , CSS, DOM and REACT</p>
-        </div>
+    <div className="project-header">
+      <div className="project-text">
+        <h1 >My Projects</h1>
+        <p >
+          Here are my Projects. Projects are done in HTML , CSS, DOM and REACT
+        </p>
+      </div>
+      <div className="projects-list">
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
