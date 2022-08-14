1ed12613f23be6a29d19cbcb881bf47ffcefd148
sgujuluva <sanghee04@gmail.com>
2022-08-14 17:18:07 +0200

    styling foto

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,29 +1,36 @@
-import React from 'react'
+import React from "react";
 //style
 import "./Intro.css";
-
+//images
+import Myfoto from "../../images/foto1.png"
 function Intro() {
   return (
     <div className="intro-container">
-     <div className="intro-left">
-        <h2 className="intro-title">Hi, I am </h2>
-        <h1 className="intro-name">Sangeetha Gujuluva</h1>
-        <div className="my-title">
+      <div className="intro-left-wrapper">
+        <div className="intro-left">
+          <h2 className="intro-title">Hi, I am </h2>
+          <h1 className="intro-name">Sangeetha Gujuluva</h1>
+          <div className="my-title">
             <div className="my-title-wrapper">
-                <div className="my-title-item">Web Developer</div>
-                <div className="my-title-item">
-                    Passionate Cook
-                </div>
-                <div className="my-title-item">
-                    Gardener
-                </div>
-                <div className="my-title-item">Event Organiser</div>
+              <div className="my-title-item">Web Developer</div>
+              <div className="my-title-item">Passionate Cook</div>
+              <div className="my-title-item">Gardener</div>
+              <div className="my-title-item">Event Organiser</div>
             </div>
+          </div>
+          <div className="my-desc">
+            I am new bee in developing websites and have been loving every
+            minute of it. I am student in Digital Carrier Institute, Germany
+            currently doing REACT and Node JS{" "}
+          </div>
         </div>
-     </div>
-     <div className="intro-right">right</div>
+      </div>
+      <div className="intro-right">
+       <div className="i-bg"></div>
+        <img src={Myfoto} alt="" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Intro
+export default Intro;
