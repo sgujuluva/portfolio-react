cf4d5f270ccceb8fac4ca095e62a8d11f4890739
sgujuluva <sanghee04@gmail.com>
2022-08-15 12:45:40 +0200

    styling contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 //styles
 import "./Contact.css"
+//images
+import Phone from "../../images/phone.png";
+import Email from "../../images/email.png";
+import Address from "../../images/address.png";
+
 function Contact() {
   return (
     <div className = "contact">
+        <div className="contact-title">
+            <h1>Contact Me</h1>
+        </div>
 <div className="contact-wrapper">
     <div className="contact-left">
         <div className="contact-info">
             <div className="contact-info-icon">
-                <img src="" alt="phone" />0 17634402690
+                <img src={Phone} alt="phone" />0 17634402690
             </div>
             <div className="contact-info-icon">
-                <img src="" alt="email" />sanghee04@gmail.com
+                <img src={Email} alt="email" />sanghee04@gmail.com
             </div>
             <div className="contact-info-icon">
-                <img src="" alt="address" />Köln
+                <img src={Address} alt="address" />Köln
             </div>
         </div>
     </div>
