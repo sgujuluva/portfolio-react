53a44b222e89077faf8e481515c4f326d22fe1b3
sgujuluva <sanghee04@gmail.com>
2022-08-15 11:22:55 +0200

    craedted  link for images with respect to projects

---
+++ src/components/ProjectList/ProjectList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+//styles
+import "./ProjectList.css"
+//data import
+import ProjectData from "../../data";
+function ProjectList() {
+  return (
+    
+        <div className="project-header">
+      <div className="project-text">
+        <h1>My Projects</h1>
+        <p>Projects are done in HTML , CSS, DOM and REACT</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectList
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,34 +1,22 @@
 import React from "react";
 //styles
 import "./Projects.css";
-//data import
-import ProjectData from "../../data";
 
-function Projects() {
+function Projects({img,link}) {
   return (
-    <div className="project-header">
-      <div className="project-text">
-        <h1 >My Projects</h1>
-        <p >
-          Projects are done in HTML , CSS, DOM and REACT
-        </p>
-      </div>
-      <div className="projects-list">
-    <div className="card">
+    
+      <div className="projects">
         <div className="browser">
-            <div className="circle"></div>
-            <div className="circle"></div>
-            <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
         </div>
-     <div className="project-images">
-     {/*  <a href="https://github.com/sgujuluva/weather-app" target="_blank" rel="noreferrer">
-        <img className="images" src={WeatherImage} alt="weather" />
-      </a> */}
-      {ProjectData.map(item => {item.link})}
-     </div>
-    </div>
+
+        <a href={link} target="_blank" rel="noreferrer">
+          <img className="images" src={img} alt="pics" />
+        </a>
       </div>
-    </div>
+   
   );
 }
 
