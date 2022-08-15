7c7e96c0fb87f0f3b9254b6b2d48e7377a7205bb
sgujuluva <sanghee04@gmail.com>
2022-08-15 13:11:27 +0200

    contact page styled

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 //styles
-import "./Contact.css"
+import "./Contact.css";
 //images
 import Phone from "../../images/phone.png";
 import Email from "../../images/email.png";
@@ -8,28 +8,41 @@ import Address from "../../images/address.png";
 
 function Contact() {
   return (
-    <div className = "contact">
-        <div className="contact-title">
-            <h1>Contact Me</h1>
+    <div className="contact">
+      <div className="contact-title">
+        <h1>Contact Me</h1>
+      </div>
+      <div className="contact-wrapper">
+        <div className="contact-left">
+          <div className="contact-info">
+          <h2> You can connect me @</h2> 
+            <div className="contact-info-icon">
+              <img src={Phone} alt="phone" />0 17634402690
+            </div>
+            <div className="contact-info-icon">
+              <img src={Email} alt="email" />
+              sanghee04@gmail.com
+            </div>
+            <div className="contact-info-icon">
+              <img src={Address} alt="address" />
+              Köln
+            </div>
+          </div>
         </div>
-<div className="contact-wrapper">
-    <div className="contact-left">
-        <div className="contact-info">
-            <div className="contact-info-icon">
-                <img src={Phone} alt="phone" />0 17634402690
+        <div className="contact-right">
+            <div className="contact-user-text">
+                Kindly Leave your details here:
             </div>
-            <div className="contact-info-icon">
-                <img src={Email} alt="email" />sanghee04@gmail.com
-            </div>
-            <div className="contact-info-icon">
-                <img src={Address} alt="address" />Köln
-            </div>
+          <form>
+            <input type="text" name="user_name" placeholder="Name" />
+            <input type="email" name="user_email" placeholder="Email" />
+            <textarea rows="7" cols="20" name="comment">Enter your message here...</textarea>
+            <button>Submit</button>
+          </form>
         </div>
+      </div>
     </div>
-    <div className="contact-right"></div>
-</div>
-    </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
