185cafc9e6ff0ac52f633d8aa9ff97168332869f
sgujuluva <sanghee04@gmail.com>
2022-08-15 11:35:31 +0200

    mapping done for projects

--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
 //styles
-import "./ProjectList.css"
+import "./ProjectList.css";
+//components
+import Projects from "../Projects/Projects";
 //data import
-import ProjectData from "../../data";
+import { projects } from "../../data";
+
 function ProjectList() {
   return (
-    
-        <div className="project-header">
+    <div className="project-header">
       <div className="project-text">
         <h1>My Projects</h1>
-        <p>Projects are done in HTML , CSS, DOM and REACT</p>
+        <p>
+          Here are my collection of Projects done with HTML, CSS, DOM and REACT
+        </p>
+      </div>
+      <div className="projects-list">
+        {projects.map(item =>(<Projects key={item.id} img={item.img} link={item.link} />))}
       </div>
     </div>
-  )
+  );
 }
 
-export default ProjectList
+export default ProjectList;
