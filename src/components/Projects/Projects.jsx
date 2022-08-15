939ceb7688c7b4bd3b3d154dcd4fd641ff016d5a
sgujuluva <sanghee04@gmail.com>
2022-08-15 10:46:44 +0200

    creating project list

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 //styles
 import "./Projects.css";
-
+//images
+import WeatherImage from "../../images/weather5.png"
 function Projects() {
   return (
     <div className="project-header">
@@ -18,7 +19,11 @@ function Projects() {
             <div className="circle"></div>
             <div className="circle"></div>
         </div>
-     
+     <div className="project-images">
+      <a href="https://github.com/sgujuluva/weather-app" target="_blank" rel="noreferrer">
+        <img className="images" src={WeatherImage} alt="weather" />
+      </a>
+     </div>
     </div>
       </div>
     </div>
---
+++ src/data.js
@@ -0,0 +1,31 @@
+//images
+import WeatherImage from "./images/weather5.png"
+import eShop from "./images/shopping.png"
+
+export const projects=[
+    {
+        id:1,
+        img : {WeatherImage},
+        link: "https://github.com/sgujuluva/weather-app"
+    },
+    {
+        id:2,
+        img : {eShop },
+        link: "https://github.com/sgujuluva/ecommerce-shopping-react"
+    },
+    {
+        id:3,
+        img : {WeatherImage},
+        link: "https://github.com/sgujuluva/weather-app"
+    },
+    {
+        id:4,
+        img : {WeatherImage},
+        link: "https://github.com/sgujuluva/weather-app"
+    },
+    {
+        id:5,
+        img : {WeatherImage},
+        link: "https://github.com/sgujuluva/weather-app"
+    },
+]
