8b49d9e845f232cc7027d2665f3bb68d9a06c4b1
sgujuluva <sanghee04@gmail.com>
2022-08-15 12:36:07 +0200

    creating contact details in contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+//styles
+import "./Contact.css"
 function Contact() {
   return (
-    <div>
-        
+    <div className = "contact">
+<div className="contact-wrapper">
+    <div className="contact-left">
+        <div className="contact-info">
+            <div className="contact-info-icon">
+                <img src="" alt="phone" />0 17634402690
+            </div>
+            <div className="contact-info-icon">
+                <img src="" alt="email" />sanghee04@gmail.com
+            </div>
+            <div className="contact-info-icon">
+                <img src="" alt="address" />Köln
+            </div>
+        </div>
+    </div>
+    <div className="contact-right"></div>
+</div>
     </div>
   )
 }
