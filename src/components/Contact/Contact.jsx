e3043220affcd79417fd3f2c5c5f250370e7fcba
sgujuluva <sanghee04@gmail.com>
2022-08-15 13:16:31 +0200

    adding submit function for form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,8 +5,13 @@ import "./Contact.css";
 import Phone from "../../images/phone.png";
 import Email from "../../images/email.png";
 import Address from "../../images/address.png";
-
+//hooks
+import {useRef} from "react"
 function Contact() {
+    const formReference = useRef();
+    const handleSubmit = (e) => {
+
+    }
   return (
     <div className="contact">
       <div className="contact-title">
@@ -33,7 +38,7 @@ function Contact() {
             <div className="contact-user-text">
                 Kindly Leave your details here:
             </div>
-          <form>
+          <form ref={formReference} onSubmit={handleSubmit}>
             <input type="text" name="user_name" placeholder="Name" />
             <input type="email" name="user_email" placeholder="Email" />
             <textarea rows="7" cols="20" name="comment">Enter your message here...</textarea>
