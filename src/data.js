98dae4f482b867f27f855228d94f6bd6fa24f37d
sgujuluva <sanghee04@gmail.com>
2022-08-17 16:09:00 +0200

    responsive styling almost done

--- src/data.js
+++ src/data.js
@@ -1,60 +1,59 @@
 //images
-import WeatherImage from "./images/weather5.png"
+import WeatherImage from "./images/weather5.png";
 import eShop from "./images/shopping.png";
 import FoodImage from "./images/food.png";
 import Todolist from "./images/todolist2.png";
 import IndianPost from "./images/postal.png";
 import CameraImage from "./images/camera.png";
-import BookSearch from "./images/booksearch.png"
-import ImageCarousel from "./images/carousel.png"
- export const projects=[
-    {
-        id:1,
-        img : WeatherImage,
-        link: "https://sangi-weather-app.vercel.app/",
-        title:"Weather App"
-    },
-    {
-        id:2,
-        img : eShop,
-        link: "https://sgujuluva-eshop.vercel.app/",
-        title:"eShopping App"
-    },
-    {
-        id:3,
-        img : FoodImage ,
-        link: "https://sgujuluva-foodingredients-app.vercel.app/",
-        title:"Food Recipe App"
-    },
-    {
-        id:4,
-        img : Todolist,
-        link: "https://github.com/sgujuluva/to-do-list-DOM",
-        title:"To-Do-List App"
-    },
-    {
-        id:5,
-        img : CameraImage,
-        link: "https://github.com/sgujuluva/camera-app-react",
-        title: "Camera App"
-    },
-    {
-        id:6,
-        img :  IndianPost,
-        link: "https://sgujuluva-indianpostal-app.vercel.app/",
-        title:"IndianPostal App"
-    },
-    {
-        id:7,
-        img :  BookSearch,
-        link: "https://sgujuluva-books-app.vercel.app/",
-        title: "Books Search App"
-    },
-    {
-        id:8,
-        img :  ImageCarousel,
-        link: "https://sgujuluva-image-carousel.vercel.app/",
-        title: "ImageCarousel"
-    },
-]
-
+import BookSearch from "./images/booksearch.png";
+import ImageCarousel from "./images/carousel.png";
+export const projects = [
+  {
+    id: 1,
+    img: WeatherImage,
+    link: "https://sangi-weather-app.vercel.app/",
+    title: "Weather App",
+  },
+  {
+    id: 2,
+    img: eShop,
+    link: "https://sgujuluva-eshop.vercel.app/",
+    title: "eShopping App",
+  },
+  {
+    id: 3,
+    img: FoodImage,
+    link: "https://sgujuluva-foodingredients-app.vercel.app/",
+    title: "Food Recipe App",
+  },
+  {
+    id: 4,
+    img: Todolist,
+    link: "https://github.com/sgujuluva/to-do-list-DOM",
+    title: "To-Do-List App",
+  },
+  {
+    id: 5,
+    img: CameraImage,
+    link: "https://sgujuluva-camera-app.vercel.app/",
+    title: "Camera App",
+  },
+  {
+    id: 6,
+    img: IndianPost,
+    link: "https://sgujuluva-indianpostal-app.vercel.app/",
+    title: "IndianPostal App",
+  },
+  {
+    id: 7,
+    img: BookSearch,
+    link: "https://sgujuluva-books-app.vercel.app/",
+    title: "Books Search App",
+  },
+  {
+    id: 8,
+    img: ImageCarousel,
+    link: "https://sgujuluva-image-carousel.vercel.app/",
+    title: "ImageCarousel",
+  },
+];
