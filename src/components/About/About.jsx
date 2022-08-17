c924b39462abf90ada3b5bfcd40b2b55ff2a941c
sgujuluva <sanghee04@gmail.com>
2022-08-17 13:18:01 +0200

    projects and projectlist styled

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 //styles
 import "./About.css";
 //image
@@ -10,27 +10,33 @@ import LeftArrow from "../../images/leftarrow.png";
 function About() {
   return (
     <div className="about-container">
-       <div className="about-left">
+      <div className="about-left">
         <img src={Collage} alt="collage" />
-       
-       </div>
-       <div className="about-right">
+      </div>
+      <div className="about-right">
         <h1>About Me</h1>
-       <p className="desc">
-        My name is Sangeetha, located in Köln Germany. I have been graduated in 2004, India with bachelor's degree in Electronics and Communication Engineering. <br/>
-       After encountering in various fields as a Teacher,Sales Manager, currently in my dream field Web Developing creating Websites. <br/>
-            I am intrested in learning, experimenting new things. <br/>
-            Team player, Easygoing, Easily adjustable to new environment.
-            </p>
-       </div>
-       <div className="arrow">
-       <Link to="/"> <img src={LeftArrow} alt="left arrow" className="left-arrow" /></Link>
-       <Link to="/Project"> <img src={RightArrow} alt="right arrow" className="right-arrow" /></Link>
-       </div>
-      
-    
-        </div>
-  )
+        <p className="desc">
+          My name is Sangeetha, located in Köln Germany. I have been graduated
+          in 2004, India with bachelor's degree in Electronics and Communication
+          Engineering. <br />
+          After encountering in various fields as a Teacher,Sales Manager,
+          currently in my dream field Web Developing creating Websites. <br />
+          I am intrested in learning, experimenting new things. <br />
+          Team player, Easygoing, Easily adjustable to new environment.
+        </p>
+      </div>
+      <div className="arrow">
+        <Link to="/">
+          
+          <img src={LeftArrow} alt="left arrow" className="left-arrow" />
+        </Link>
+        <Link to="/Project">
+         
+          <img src={RightArrow} alt="right arrow" className="right-arrow" />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
