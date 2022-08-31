2374f5a81fac2efcaddae34321e8469a93cda5b1
sgujuluva <sanghee04@gmail.com>
2022-08-31 14:34:47 +0200

    netflix app added

--- src/data.js
+++ src/data.js
@@ -7,58 +7,67 @@ import IndianPost from "./images/postal.png";
 import CameraImage from "./images/camera.png";
 import BookSearch from "./images/booksearch.png";
 import ImageCarousel from "./images/carousel.png";
+import TodolistReact from "./images/todoreact.png";
+
 export const projects = [
+  
   {
     id: 1,
+    img: WeatherImage,
+    link: "https://sgujuluva-netflix-clone-react.vercel.app",
+    title: "Netflix-Clone React",
+  },
+  {
+    id: 2,
     img: WeatherImage,
     link: "https://sangi-weather-app.vercel.app/",
     title: "Weather App",
   },
   {
-    id: 2,
+    id: 3,
     img: eShop,
     link: "https://sgujuluva-eshop.vercel.app/",
     title: "eShopping App",
   },
   {
-    id: 3,
+    id: 4,
     img: FoodImage,
     link: "https://sgujuluva-foodingredients-app.vercel.app/",
     title: "Food Recipe App",
   },
   {
-    id: 4,
+    id: 5,
     img: Todolist,
     link: "https://sgujuluva.github.io/to-do-list-DOM/",
     title: "To-Do-List App",
   },
   {
-    id: 5,
+    id: 6,
     img: CameraImage,
     link: "https://sgujuluva-camera-app.vercel.app/",
     title: "Camera App",
   },
   {
-    id: 6,
+    id: 7,
     img: IndianPost,
     link: "https://sgujuluva-indianpostal-app.vercel.app/",
     title: "IndianPostal App",
   },
   {
-    id: 7,
+    id: 8,
     img: BookSearch,
     link: "https://sgujuluva-books-app.vercel.app/",
     title: "Books Search App",
   },
   {
-    id: 8,
+    id: 9,
     img: ImageCarousel,
     link: "https://sgujuluva-image-carousel.vercel.app/",
     title: "ImageCarousel",
   },
   {
-    id: 9,
-    img: ImageCarousel,
+    id: 10,
+    img: TodolistReact,
     link: "  https://sgujuluva-todolist-react.vercel.app/",
     title: "To-do-list REACT",
   },
