7598af5649fd0596db3641e8d082bb281d9fca88
sgujuluva <sanghee04@gmail.com>
2022-09-01 16:55:39 +0200

    intro page styling

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -23,15 +23,19 @@ function Intro() {
               </div>
             </div>
             <p className="my-desc">
-            I am a enthusiastic MERN Stack web developer, who is about to graduate a one year Intensive Web Dev Training. I am seeking to obtain a creative and challenging position that utilizes current knowledge of web development and design. Having a positive outlook and am always willing to learn new traits. I work well both independently and as part of a team.
+              I am an enthusiastic MERN Stack web developer, who is about to
+              graduate a one year Intensive Web Dev Training. I am seeking to
+              obtain a creative and challenging position that utilizes current
+              knowledge of web development and design. Having a positive outlook
+              and am always willing to learn new traits. I work well both
+              independently and as part of a team.
             </p>
-            <Link to="/about">
-              <img src={RightArrow} alt="right arrow" className="right-arrow" />
-            </Link>
+            <div className="right-arrow">
+              <Link to="/about">
+                <img src={RightArrow} alt="right arrow" />
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className="intro-right">
-          {/* <div className="i-bg"><img src={Myfoto} alt="my foto" /></div> */}
         </div>
       </div>
     </>
