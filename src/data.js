589a2885d59eeabf22d09b1c29c9bc7fc8e94d55
sgujuluva <sanghee04@gmail.com>
2022-09-16 17:23:15 +0200

    clone added

--- src/data.js
+++ src/data.js
@@ -30,7 +30,7 @@ export const projects = [
     id: 3,
     img: NikeShoe,
     link: "https://sgujuluva-shoes-app-react.vercel.app/",
-    title: "Nike Shoe - React App",
+    title: "Nike Shoe-Clone- React App",
   },
   {
     id: 4,
